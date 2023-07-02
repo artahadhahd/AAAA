608c89fc4aa947b2b69634c29c4c06aa8b5e8674
artahadhahd <iamarta.azadegan@gmail.com>
2023-07-02 22:38:27 +0200

    refactored instlal script

--- install.py
+++ install.py
@@ -16,6 +16,12 @@ WARNINGS = "-Werror -Wall -Wextra -Wno-unused-variable -Wno-unused-parameter"
 INCLUDE = "-Iinclude -Ilib/win32"
 DLLS = "-lopengl32 -lgdi32 -lSDL2"
 
+def get_help():
+    print("Available commands:")
+    print("install.py             => Compile for your platform")
+    print("install.py -g [ 0..3 ] => Compile for debugging (meant for developers)")
+    print("install.py -h          => Show this help message")
+
 def compile(*, debug_flags: str):
     if os.name == 'nt':
         if os.system("gcc --version >nul 2>&1"):
@@ -23,16 +29,17 @@ def compile(*, debug_flags: str):
             sys.exit(1)
         if not Path('bin').exists():
             os.mkdir('bin')
-        if Path('C:\Program Files (x86)').exists(): # if 32-bit
-            output = os.system(f'gcc {debug_flags} src/*.c -o bin/PPM.exe {INCLUDE} -Llib/win32/SDL2/32 -lSDL2 {WARNINGS}')
-        else:
-            output = os.system(f'gcc {debug_flags} src/*.c -o bin/PPM.exe {INCLUDE} -Llib/win32/SDL2/64 -lSDL2 {WARNINGS}')
+        a = f'gcc {debug_flags} src/*.c -o bin/PPM.exe {INCLUDE} -Llib/win32/SDL2/{[64, 32][Path("C:/Program Files (x86)").exists()]} {DLLS} {WARNINGS}'
+        print(a)
+        output = os.system(a)
         if output:
             print("\n---------\nCompilation failed. See above for more details. Report bugs to https://github.com/artahadhahd/AAAA")
             sys.exit(1)
+    print("Compiled successfully. Executable in bin/PPM.exe")
 
 if len(option := sys.argv) != 1:
-    if option[1].lower() == 'debug':
+    first_arg = option[1].lower()
+    if first_arg == '-g':
         print("Compiling for debug mode")
         if len(option) == 3:
             if int(option[2]) < 0 or int(option[2]) > 3:
@@ -44,6 +51,9 @@ if len(option := sys.argv) != 1:
             os.system('gdb bin/PPM.exe')
         else:
             sys.exit(0)
+    if first_arg == '-h':
+        get_help()
+        sys.exit(0)
     else:
         print("Invalid option")
 compile(debug_flags='')
